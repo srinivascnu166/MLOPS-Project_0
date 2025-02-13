f490fde7dd1b960ea7460a16aa9623d6d375a6b1
Srinivas akuthota <srinivascnu166@gmail.com>
2025-02-13 22:41:36 +0530

    vehicle insurance structure is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="srinivas akuthota",
+    author_email="srinivascnu166@gmail.com",
+    packages=find_packages()
+)
